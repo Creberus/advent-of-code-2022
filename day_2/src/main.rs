e98b5498383c7dc430b4519d3bf3aaf5d2862c8a
Creberus <creberus@tutanota.com>
2022-12-02 12:25:52 +0100

    day2: Add second part implementation

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -5,6 +5,10 @@ const LOSE: u32 = 0;
 const DRAW: u32 = 3;
 const WIN: u32 = 6;
 
+const ROCK: u32 = 1;
+const PAPER: u32 = 2;
+const SCISSORS: u32 = 3;
+
 /// Opponent
 /// A => Rock
 /// B => Paper
@@ -48,7 +52,29 @@ impl From<char> for Player {
     }
 }
 
-fn fight(opponent: Opponent, player: Player) -> u32 {
+/// Player
+/// Y => DRAW
+/// X => LOSE
+/// Z => WIN
+
+enum RoundStatus {
+    LOSE,
+    DRAW,
+    WIN,
+}
+
+impl From<char> for RoundStatus {
+    fn from(c: char) -> Self {
+        match c {
+            'X' => RoundStatus::LOSE,
+            'Y' => RoundStatus::DRAW,
+            'Z' => RoundStatus::WIN,
+            _ => panic!("Opponent input is invalid"),
+        }
+    }
+}
+
+fn fight_part_one(opponent: Opponent, player: Player) -> u32 {
     match opponent {
         Opponent::Rock => match player {
             Player::Rock => 1 + DRAW,
@@ -68,6 +94,26 @@ fn fight(opponent: Opponent, player: Player) -> u32 {
     }
 }
 
+fn fight_part_two(opponent: Opponent, round_status: RoundStatus) -> u32 {
+    match opponent {
+        Opponent::Rock => match round_status {
+            RoundStatus::WIN => WIN + PAPER,
+            RoundStatus::DRAW => DRAW + ROCK,
+            RoundStatus::LOSE => LOSE + SCISSORS,
+        },
+        Opponent::Paper => match round_status {
+            RoundStatus::WIN => WIN + SCISSORS,
+            RoundStatus::DRAW => DRAW + PAPER,
+            RoundStatus::LOSE => LOSE + ROCK,
+        },
+        Opponent::Scissors => match round_status {
+            RoundStatus::WIN => WIN + ROCK,
+            RoundStatus::DRAW => DRAW + SCISSORS,
+            RoundStatus::LOSE => LOSE + PAPER,
+        },
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let lines = io::stdin().lines();
     let mut score: u32 = 0;
@@ -77,11 +123,22 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         let mut it = l.chars();
 
+        // Part 1
+        /*
         let opponent: Opponent = Opponent::from(it.next().expect("Invalid Opponent input"));
         let _ = it.next();
         let player: Player = Player::from(it.next().expect("Invalid Player input."));
 
-        let round = fight(opponent, player);
+        let round = fight_part_one(opponent, player);
+        */
+
+        // Part 2
+        let opponent: Opponent = Opponent::from(it.next().expect("Invalid Opponent input"));
+        let _ = it.next();
+        let round_status: RoundStatus =
+            RoundStatus::from(it.next().expect("Invalid Player input."));
+
+        let round = fight_part_two(opponent, round_status);
 
         score += round;
     }
