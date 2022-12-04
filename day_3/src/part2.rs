3a9573a1266d7135297082e244cff27b9fd522ca
Creberus <creberus@tutanota.com>
2022-12-04 17:54:04 +0100

    day_3: Add part2 implementation
    Split structure for the 2 parts.

---
+++ day_3/src/part2.rs
@@ -0,0 +1,81 @@
+use std::collections::HashSet;
+use std::error::Error;
+use std::io;
+
+pub fn main_p2() -> Result<(), Box<dyn Error>> {
+    let mut lines = io::stdin().lines();
+    let mut priority: u32 = 0;
+
+    loop {
+        // Break condition
+        let first = lines.next();
+        if first.is_none() {
+            break;
+        }
+
+        let first = first.unwrap().unwrap();
+        let second = lines.next().unwrap().unwrap();
+        let third = lines.next().unwrap().unwrap();
+
+        priority += compute(first, second, third);
+    }
+
+    println!("Part2: Total priority: {}", priority);
+
+    Ok(())
+}
+
+fn compute(first: String, second: String, third: String) -> u32 {
+    let inputs = [first, second, third];
+
+    let mut rucksacks = [HashSet::new(), HashSet::new(), HashSet::new()];
+
+    for i in 0..3 {
+        inputs[i].chars().into_iter().for_each(|c| {
+            let _ = rucksacks[i].insert(c);
+            ()
+        });
+    }
+
+    let one_two: HashSet<char> = rucksacks[0].intersection(&rucksacks[1]).cloned().collect();
+    let two_three: HashSet<char> = one_two.intersection(&rucksacks[2]).cloned().collect();
+
+    assert!(two_three.len() == 1);
+
+    let value = two_three.iter().nth(0).unwrap();
+
+    if value.is_lowercase() {
+        *value as u32 - 'a' as u32 + 1
+    } else if value.is_uppercase() {
+        *value as u32 - 'A' as u32 + 27
+    } else {
+        panic!("Character not recognized");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::compute;
+
+    #[test]
+    fn simple() {
+        let first = String::from("vJrwpWtwJgWrhcsFMMfFFhFp");
+        let second = String::from("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL");
+        let third = String::from("PmmdzqPrVvPwwTWBwg");
+
+        let value = compute(first, second, third);
+
+        assert_eq!(value, 18);
+    }
+
+    #[test]
+    fn simple_second() {
+        let first = String::from("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn");
+        let second = String::from("ttgJtRGJQctTZtZT");
+        let third = String::from("CrZsJsPPZsGzwwsLwLmpwMDw");
+
+        let value = compute(first, second, third);
+
+        assert_eq!(value, 52);
+    }
+}
