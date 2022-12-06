6aa7153161decdc900cbcaeb3a0627af57355bc7
Creberus <creberus@tutanota.com>
2022-12-06 01:35:32 +0100

    day_1: Refacto code for part one with better impl and tests

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,32 +1,8 @@
 use std::error::Error;
-use std::io;
+
+mod part_one;
+use part_one::main_p1;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let lines = io::stdin().lines();
-
-    let mut elves = Vec::<u32>::new();
-    elves.push(0);
-
-    for line in lines {
-        let l = line.unwrap();
-
-        if l.is_empty() {
-            elves.push(0);
-        } else {
-            if let Some(last) = elves.last_mut() {
-                *last += l.parse::<u32>().unwrap();
-            }
-        }
-    }
-
-    println!("Max: {:?}", elves.iter().max());
-
-    elves.sort();
-    let (_, right) = elves.split_at(elves.len() - 3);
-
-    let maxs: u32 = right.iter().sum();
-
-    println!("Max 3: {:?}", maxs);
-
-    Ok(())
+    main_p1()
 }
