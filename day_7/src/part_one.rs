008870b43837c4fc37ded60e2d19c01267c412d6
Creberus <creberus@tutanota.com>
2022-12-07 22:51:35 +0100

    main: Add parsing logic from input. Add Tree structure for File System.
    main: Add 2 logic for commands and info from ls.
    tree: Add Tree structure in order to ease implementation and utilization
    of the small file system.

--- day_7/src/part_one.rs
+++ day_7/src/part_one.rs
@@ -4,7 +4,26 @@ use std::io;
 pub fn main_p1() -> Result<(), Box<dyn Error>> {
     let mut lines = io::stdin().lines();
 
-    let line = lines.next().unwrap().unwrap();
+    for line in lines {
+        let line = line.unwrap();
+
+        if line.starts_with("$") {
+            // Command parsing
+            let command: Vec<&str> = line.split(' ').collect();
+            match command[0] {
+                "cd" => (), //TODO: Change current working directory
+                _ => (),
+            };
+        } else {
+            // Directory and File reading from `ls`
+            let info: Vec<&str> = line.split(' ').collect();
+
+            match info[0] {
+                "dir" => (), //TODO: Create Dir
+                _ => (),     //TODO: Create File
+            }
+        }
+    }
 
     Ok(())
 }
--- day_7/src/tree.rs
+++ day_7/src/tree.rs
@@ -1,3 +1,22 @@
+pub struct Tree {
+    childs: Vec<Box<dyn Node>>,
+}
+
+impl Tree {
+    pub fn new() -> Self {
+        Tree { childs: Vec::new() }
+    }
+
+    pub fn add(path: String, node: Box<dyn Node>) -> Result<(), ()> {
+        // TODO: Implement add logic
+        // node correspond to the full path for a node.
+        // Example:
+        // For a file: path = "/a/", node = File::new(...)
+        // For a dir: path = "/my/sub/", node = Dir::new(...)
+        Ok(())
+    }
+}
+
 pub enum NodeType {
     DIRECTORY,
     FILE,
@@ -5,8 +24,9 @@ pub enum NodeType {
 
 pub trait Node {
     fn name(&self) -> String;
-    fn get_type(&self) -> NodeType;
     fn size(&self) -> usize;
+    fn get_type(&self) -> NodeType;
+    fn add_child(&mut self, child: Box<dyn Node>) -> Result<(), ()>;
 }
 
 pub struct Dir {
@@ -23,6 +43,30 @@ impl Dir {
     }
 }
 
+impl Node for Dir {
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    fn size(&self) -> usize {
+        let mut size: usize = 0;
+
+        for node in &self.childs {
+            size += node.size()
+        }
+
+        size
+    }
+
+    fn get_type(&self) -> NodeType {
+        NodeType::DIRECTORY
+    }
+
+    fn add_child(&mut self, child: Box<dyn Node>) -> Result<(), ()> {
+        Ok(())
+    }
+}
+
 pub struct File {
     name: String,
     size: usize,
@@ -31,5 +75,23 @@ pub struct File {
 impl File {
     pub fn new(name: String, size: usize) -> Self {
         File { name, size }
+    }
+}
+
+impl Node for File {
+    fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    fn size(&self) -> usize {
+        self.size
+    }
+
+    fn get_type(&self) -> NodeType {
+        NodeType::FILE
+    }
+
+    fn add_child(&mut self, child: Box<dyn Node>) -> Result<(), ()> {
+        Err(())
     }
 }
