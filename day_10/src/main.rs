e9235bc8e170cc548bd7fc2ab8aa1ad0a1107d59
Creberus <creberus@tutanota.com>
2022-12-10 19:09:11 +0100

    day_10: Add second part implementation

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -1,12 +1,12 @@
 mod part_one;
-//mod part_two;
+mod part_two;
 
 use part_one::main_p1;
-//use part_two::main_p2;
+use part_two::main_p2;
 
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    main_p1()
-    //main_p2()
+    //main_p1()
+    main_p2()
 }
