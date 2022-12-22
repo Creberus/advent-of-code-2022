796ae29fc3edec5275593b8c0b787ac0e7d968e2
Creberus <creberus@tutanota.com>
2022-12-22 14:23:36 +0100

    day_15: Add part_one implementation

--- day_15/src/part_one.rs
+++ day_15/src/part_one.rs
@@ -1,10 +1,12 @@
+use std::collections::{HashMap, VecDeque};
 use std::error::Error;
 use std::io;
 
 pub fn main_p1() -> Result<(), Box<dyn Error>> {
     let lines = io::stdin().lines();
 
-    let mut items = Vec::<Item>::new();
+    let mut items = Vec::<(Item, Item)>::new();
+    let mut map = Map::new();
 
     for line in lines {
         let line = line.unwrap();
@@ -17,16 +19,66 @@ pub fn main_p1() -> Result<(), Box<dyn Error>> {
         let sensor = Item::parse(sensor);
         let beacon = Item::parse(beacon);
 
-        items.push(sensor);
-        items.push(beacon);
+        items.push((sensor, beacon));
     }
 
     items.iter().for_each(|i| println!("{:?}", i));
 
+    // For each couple of Sensor/Beacon, fill the map with waves from the sensor to the beacon in
+    // circular shape.
+    for (sensor, beacon) in items {
+        let mut q = VecDeque::<Point>::new();
+        let p = sensor.point();
+        let mut beacon_found = false;
+        let mut p_left = 1;
+
+        map.insert(sensor.point(), sensor.t());
+        map.insert(beacon.point(), beacon.t());
+
+        q.push_back(p);
+
+        while !q.is_empty() {
+            let p = q.pop_front().unwrap();
+            map.insert(p, Type::Scanned);
+
+            if let Some(beacon) = map.has(&p) {
+                if *beacon == Type::Beacon {
+                    beacon_found = true;
+                }
+            }
+
+            let up = Point::new(p.x(), p.y() - 1);
+            let down = Point::new(p.x(), p.y() + 1);
+            let left = Point::new(p.x() - 1, p.y());
+            let right = Point::new(p.x() + 1, p.y());
+
+            q.push_back(up);
+            q.push_back(down);
+            q.push_back(left);
+            q.push_back(right);
+
+            p_left -= 1;
+            if p_left == 0 {
+                p_left = q.len();
+                if beacon_found {
+                    break;
+                }
+            }
+        }
+    }
+
+    // Look for row y=2_000_000
+    let beacant = map.check(10);
+
+    println!(
+        "There are {} positions where a beacon cannot be present",
+        beacant
+    );
+
     Ok(())
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Point {
     x: i32,
     y: i32,
@@ -48,12 +100,21 @@ impl Point {
 
         Point::new(x, y)
     }
+
+    fn x(&self) -> i32 {
+        self.x
+    }
+
+    fn y(&self) -> i32 {
+        self.y
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Type {
     Sensor,
     Beacon,
+    Scanned,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -85,6 +146,67 @@ impl Item {
         } else {
             panic!("Can't parse item.");
         }
+    }
+
+    fn point(&self) -> Point {
+        self.point
+    }
+
+    fn t(&self) -> Type {
+        self.t
+    }
+}
+
+#[derive(Debug)]
+struct Map {
+    map: HashMap<Point, Type>,
+    min_x: i32,
+    max_x: i32,
+    min_y: i32,
+    max_y: i32,
+}
+
+impl Map {
+    fn new() -> Self {
+        Self {
+            map: HashMap::new(),
+            min_x: i32::MAX,
+            max_x: i32::MIN,
+            min_y: i32::MAX,
+            max_y: i32::MIN,
+        }
+    }
+
+    fn insert(&mut self, p: Point, t: Type) -> bool {
+        if self.map.contains_key(&p) {
+            return false;
+        } else {
+            self.map.insert(p, t);
+            self.min_y = self.min_y.min(p.y());
+            self.min_x = self.min_x.min(p.x());
+            self.max_y = self.max_y.max(p.y());
+            self.max_x = self.max_x.max(p.x());
+            return true;
+        }
+    }
+
+    fn has(&self, p: &Point) -> Option<&Type> {
+        self.map.get(&p)
+    }
+
+    fn check(&self, row: i32) -> u32 {
+        let mut beacant = 0;
+
+        for x in self.min_x..=self.max_x {
+            if let Some(item) = self.map.get(&Point::new(x, row)) {
+                match item {
+                    Type::Beacon => continue,
+                    _ => beacant += 1,
+                }
+            }
+        }
+
+        beacant
     }
 }
 
