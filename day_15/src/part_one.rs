ec4acbcdf9accaf8b7d7b46cd07a683b812e1926
Creberus <creberus@tutanota.com>
2022-12-22 15:14:11 +0100

    day_15: Udpated part one implementation

--- day_15/src/part_one.rs
+++ day_15/src/part_one.rs
@@ -1,12 +1,11 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 use std::error::Error;
 use std::io;
 
 pub fn main_p1() -> Result<(), Box<dyn Error>> {
     let lines = io::stdin().lines();
 
-    let mut items = Vec::<(Item, Item)>::new();
-    let mut map = Map::new();
+    let mut items = HashMap::<Point, Type>::new();
 
     for line in lines {
         let line = line.unwrap();
@@ -16,63 +15,86 @@ pub fn main_p1() -> Result<(), Box<dyn Error>> {
         let sensor = line[0];
         let beacon = line[1];
 
-        let sensor = Item::parse(sensor);
-        let beacon = Item::parse(beacon);
+        let (_, sensor) = sensor.split_at(10);
+        let (_, beacon) = beacon.split_at(22);
 
-        items.push((sensor, beacon));
+        let sensor = Point::parse(sensor);
+        let beacon = Point::parse(beacon);
+
+        items.insert(sensor, Type::Sensor(sensor.compare(&beacon)));
+        items.insert(beacon, Type::Beacon);
     }
 
     items.iter().for_each(|i| println!("{:?}", i));
 
-    // For each couple of Sensor/Beacon, fill the map with waves from the sensor to the beacon in
-    // circular shape.
-    for (sensor, beacon) in items {
-        let mut q = VecDeque::<Point>::new();
-        let p = sensor.point();
-        let mut beacon_found = false;
-        let mut p_left = 1;
+    let mut min_x = 0;
+    let mut max_x = 0;
+    let mut min_y = 0;
+    let mut max_y = 0;
 
-        map.insert(sensor.point(), sensor.t());
-        map.insert(beacon.point(), beacon.t());
-
-        q.push_back(p);
-
-        while !q.is_empty() {
-            let p = q.pop_front().unwrap();
-            map.insert(p, Type::Scanned);
-
-            if let Some(beacon) = map.has(&p) {
-                if *beacon == Type::Beacon {
-                    beacon_found = true;
-                }
+    for (point, t) in &items {
+        match t {
+            Type::Beacon => {
+                min_x = min_x.min(point.x());
+                min_y = min_y.min(point.y());
+                max_x = max_x.max(point.x());
+                max_y = max_y.max(point.y());
             }
-
-            let up = Point::new(p.x(), p.y() - 1);
-            let down = Point::new(p.x(), p.y() + 1);
-            let left = Point::new(p.x() - 1, p.y());
-            let right = Point::new(p.x() + 1, p.y());
-
-            q.push_back(up);
-            q.push_back(down);
-            q.push_back(left);
-            q.push_back(right);
-
-            p_left -= 1;
-            if p_left == 0 {
-                p_left = q.len();
-                if beacon_found {
-                    break;
-                }
+            Type::Sensor(value) => {
+                min_x = min_x.min(point.x() - *value as i32);
+                min_y = min_y.min(point.y() - *value as i32);
+                max_x = max_x.max(point.x() + *value as i32);
+                max_y = max_y.max(point.y() + *value as i32);
             }
         }
     }
 
-    // Look for row y=2_000_000
-    let beacant = map.check(10);
+    println!("min_x: {}", min_x);
+    println!("max_x: {}", max_x);
+    println!("min_y: {}", min_y);
+    println!("max_y: {}", max_y);
+
+    let row = 2_000_000;
+    let mut taken = 0;
+
+    // Algorithm
+    // 1. Take a point in the map
+    // 2. Check if it's already a Beacon or a Sensor
+    // 3. Compute manathan distance for each sensor
+    // 4. Compare with manathan distance of the sensor with it's closest beacon
+    // 5. If it's higher than everyone, it's empty ! Else, it's scanned.
+    for x in min_x..=max_x {
+        // 1.
+        let p = Point::new(x, row);
+        print!("x: {}\r", x);
+
+        // 2.
+        if let Some(item) = items.get(&p) {
+            match item {
+                Type::Beacon => (),
+                Type::Sensor(_) => taken += 1,
+            }
+
+            continue;
+        }
+
+        // 3+4+5.
+        for (point, t) in &items {
+            match t {
+                Type::Sensor(value) => {
+                    if p.compare(point) <= *value {
+                        taken += 1;
+                        break;
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
 
     println!(
-        "There are {} positions where a beacon cannot be present",
-        beacant
+        "There are {} positions where a beacon cannot be present.",
+        taken
     );
 
     Ok(())
@@ -101,6 +123,10 @@ impl Point {
         Point::new(x, y)
     }
 
+    fn compare(&self, other: &Self) -> u32 {
+        (self.x - other.x).abs() as u32 + (self.y - other.y).abs() as u32
+    }
+
     fn x(&self) -> i32 {
         self.x
     }
@@ -112,102 +138,8 @@ impl Point {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Type {
-    Sensor,
+    Sensor(u32),
     Beacon,
-    Scanned,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Item {
-    t: Type,
-    point: Point,
-}
-
-impl Item {
-    fn new(t: Type, p: Point) -> Self {
-        Item { t, point: p }
-    }
-
-    fn parse(s: &str) -> Self {
-        let input: Vec<&str> = s.split(' ').collect();
-
-        if input[0] == "Sensor" {
-            let (_, coordinates) = s.split_at(10);
-
-            let p = Point::parse(coordinates);
-
-            Item::new(Type::Sensor, p)
-        } else if input[2] == "beacon" {
-            let (_, coordinates) = s.split_at(22);
-
-            let p = Point::parse(coordinates);
-
-            Item::new(Type::Beacon, p)
-        } else {
-            panic!("Can't parse item.");
-        }
-    }
-
-    fn point(&self) -> Point {
-        self.point
-    }
-
-    fn t(&self) -> Type {
-        self.t
-    }
-}
-
-#[derive(Debug)]
-struct Map {
-    map: HashMap<Point, Type>,
-    min_x: i32,
-    max_x: i32,
-    min_y: i32,
-    max_y: i32,
-}
-
-impl Map {
-    fn new() -> Self {
-        Self {
-            map: HashMap::new(),
-            min_x: i32::MAX,
-            max_x: i32::MIN,
-            min_y: i32::MAX,
-            max_y: i32::MIN,
-        }
-    }
-
-    fn insert(&mut self, p: Point, t: Type) -> bool {
-        if self.map.contains_key(&p) {
-            return false;
-        } else {
-            self.map.insert(p, t);
-            self.min_y = self.min_y.min(p.y());
-            self.min_x = self.min_x.min(p.x());
-            self.max_y = self.max_y.max(p.y());
-            self.max_x = self.max_x.max(p.x());
-            return true;
-        }
-    }
-
-    fn has(&self, p: &Point) -> Option<&Type> {
-        self.map.get(&p)
-    }
-
-    fn check(&self, row: i32) -> u32 {
-        let mut beacant = 0;
-
-        for x in self.min_x..=self.max_x {
-            if let Some(item) = self.map.get(&Point::new(x, row)) {
-                match item {
-                    Type::Beacon => continue,
-                    _ => beacant += 1,
-                }
-            }
-        }
-
-        beacant
-    }
 }
 
 #[cfg(test)]
@@ -215,20 +147,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn simple_sensor() {
-        let input = "Sensor at x=2, y=18";
-
-        let item = Item::parse(input);
-
-        assert_eq!(item, Item::new(Type::Sensor, Point::new(2, 18)));
+    fn point_pos() {
+        let input = "x=2, y=18";
     }
 
     #[test]
-    fn simple_beacon() {
-        let input = " closest beacon is at x=-2, y=15";
-
-        let item = Item::parse(input);
-
-        assert_eq!(item, Item::new(Type::Beacon, Point::new(-2, 15)));
+    fn point_neg() {
+        let input = "x=-2, y=15";
     }
 }
