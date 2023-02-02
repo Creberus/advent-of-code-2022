f8795751f818011ccba2837d818f5db9121acc13
Benjamin Peter <benjamin.peter@epita.fr>
2023-02-02 18:13:27 +0100

    day_21: part_two: Add transformation into Expr Tree

--- day_21/src/part_two.rs
+++ day_21/src/part_two.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::error::Error;
+use std::fmt::{Display, Write};
 use std::io;
 
 //TODO: Reduce the tree to have a node with : humn + <number>
@@ -36,8 +37,9 @@ pub fn main_p2() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    let result = eval_root(&monkeys);
-    println!("Result: {}", result);
+    let tree_expr = build_expr_tree(String::from("root"), &monkeys);
+
+    println!("Tree Expr: {}", tree_expr);
 
     Ok(())
 }
@@ -75,7 +77,7 @@ fn eval(expr: &Expression, monkeys: &HashMap<String, Expression>) -> i64 {
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Operator {
     Plus,
     Minus,
@@ -98,10 +100,67 @@ impl From<&str> for Operator {
     }
 }
 
+impl Display for Operator {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Operator::Plus => f.write_str("+"),
+            Operator::Minus => f.write_str("-"),
+            Operator::Mul => f.write_str("*"),
+            Operator::Div => f.write_str("/"),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Eq)]
 enum Expression {
     Operation(String, Operator, String),
     Number(i64),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum TreeExpr {
+    Number(i64),
+    Variable(),
+    Operation(Box<TreeExpr>, Operator, Box<TreeExpr>),
+    Root(Box<TreeExpr>, Box<TreeExpr>),
+}
+
+impl Display for TreeExpr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TreeExpr::Root(l, r) => f.write_fmt(format_args!("({}) == ({})", l, r)),
+            TreeExpr::Number(value) => f.write_fmt(format_args!("{}", value)),
+            TreeExpr::Variable() => f.write_str("x"),
+            TreeExpr::Operation(l, op, r) => f.write_fmt(format_args!("({} {} {})", l, op, r)),
+        }
+    }
+}
+
+fn build_expr_tree(monkey: String, monkeys: &HashMap<String, Expression>) -> TreeExpr {
+    let m = monkeys.get(&monkey).unwrap();
+
+    if *monkey == String::from("root") {
+        if let Expression::Operation(lm, _, rm) = m {
+            let lexpr = build_expr_tree(lm.clone(), monkeys);
+            let rexpr = build_expr_tree(rm.clone(), monkeys);
+
+            TreeExpr::Root(Box::new(lexpr), Box::new(rexpr))
+        } else {
+            panic!("Root is malformed");
+        }
+    } else if *monkey == String::from("humn") {
+        TreeExpr::Variable()
+    } else {
+        match m {
+            Expression::Number(value) => TreeExpr::Number(*value),
+            Expression::Operation(lm, op, rm) => {
+                let lexpr = build_expr_tree(lm.clone(), monkeys);
+                let rexpr = build_expr_tree(rm.clone(), monkeys);
+
+                TreeExpr::Operation(Box::new(lexpr), *op, Box::new(rexpr))
+            }
+        }
+    }
 }
 
 #[cfg(test)]
